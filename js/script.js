bda91bea604d4e78ddb3934a1aeb430d6a1cc62b
Olga Parfenova <work.parfenova@gmail.com>
2022-11-14 19:04:49 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(item = 0) {
+        tabsContent[item].style.display = 'block';
+        tabs[item].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+});
+
+

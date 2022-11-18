810445097d273feee82bc52147b4405e89652adb
Olga Parfenova <work.parfenova@gmail.com>
2022-11-18 16:18:15 +0300

    Added a function for the timer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    //Tabs:
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -34,6 +36,24 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //Timer:
+
+    const deadline = '2022-12-20';
+
+    function getTimeRemaining (endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor(t / (1000 * 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
 });
-
-
